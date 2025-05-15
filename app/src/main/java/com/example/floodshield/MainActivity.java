d4b18b42d8c3b4122606b6ed971d6561237e4d17
Kaustav <2004.k.c2@gmail.com>
2025-05-15 15:58:36 +0530

    First Commit

---
+++ app/src/main/java/com/example/floodshield/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.floodshield;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+//        Making Home Fragment Default
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_frame, new HomeFragment())
+                    .commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.nav_map) {
+                // Load Map Fragment
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_frame, new MapFragment())
+                        .commit();
+            } else if (id == R.id.nav_alerts) {
+                // Load Alerts Fragment
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_frame, new AlertFragment())
+                        .commit();
+            } else if (id == R.id.nav_settings) {
+                // Load Settings Fragment
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.content_frame, new SettingFragment())
+                        .commit();
+            }
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
