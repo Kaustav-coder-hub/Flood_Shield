d0210ee3ecb035bba7bae881e463464a483bc3d4
kaustav <2004.k.c2@gmail.com>
2025-05-17 16:58:39 +0530

    Map Added

--- app/src/main/java/com/example/floodshield/MapFragment.java
+++ app/src/main/java/com/example/floodshield/MapFragment.java
@@ -4,19 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class MapFragment extends Fragment {
 
-    public MapFragment() {
-        // Required empty public constructor
+    private WebView mapWebView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mapWebView = view.findViewById(R.id.mapWebView);
+
+        WebSettings webSettings = mapWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mapWebView.setWebViewClient(new WebViewClient());
+        mapWebView.loadUrl("file:///android_asset/osm_map.html");
+
+        return view;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        mapWebView.destroy();
     }
 }
